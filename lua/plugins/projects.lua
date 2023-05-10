8ff7ad8bec69f9139312880c3f033875fb724cbc
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-05-10 19:07:07 -0300

    projects.lua refactored

---
+++ lua/plugins/projects.lua
@@ -0,0 +1,13 @@
+return {
+	"ahmedkhalf/project.nvim",
+	config = function()
+		require("project_nvim").setup({
+			sync_root_with_cwd = true,
+			respect_buf_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
+		})
+	end,
+}
