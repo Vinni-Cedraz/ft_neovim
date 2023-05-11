66dfe38da36a1b39591228ab01d814ed8e1e13c7
Vinni-Cedraz <Vinni-Cedraz@gmail.com>
2023-05-11 17:00:43 -0300

    Succesfully loading copilot and vim bbye

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
 	"zbirenbaum/copilot.lua",
 	lazy = true,
+	event = "InsertCharPre",
 	config = function()
 		require("copilot").setup({
 			filetypes = { "lua", "c", "javascript", "typescript", "rust", "python" },
