2d54a959baf6c27e2c4424ed378119911a25ddc8
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-07-02 17:23:12 -0300

    added gitsigns
    with proper Lazy syntax
    
    new file:   lua/plugins/gitsigns.lua
    modified:   lua/plugins/lazy.lua
    modified:   lua/plugins/telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,17 @@ return {
 	"nvim-telescope/telescope.nvim",
 	branch = "master",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		-- See `:help telescope` and `:help telescope.setup()`
+		require("telescope").setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-u>"] = false,
+						["<C-d>"] = false,
+					},
+				},
+			},
+		})
+	end,
 }
