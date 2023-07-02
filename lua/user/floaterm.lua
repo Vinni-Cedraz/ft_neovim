d155b2b12ea68c18929643170d17cd7502d1e638
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-07-02 15:52:34 -0300

    floaterm fix

--- lua/user/floaterm.lua
+++ lua/user/floaterm.lua
@@ -3,3 +3,11 @@ vim.g.floaterm_width = 0.8
 vim.g.floaterm_height = 0.8
 vim.g.floaterm_wintitle = 0
 vim.g.floaterm_autoclose = 1
+vim.g.floaterm_borderchars = "─│─│┌┐┘└"
+
+-- if floaterm is opening with a vanilla 'sh' shell instead of your shell of
+-- choice, uncomment one of the following lines:
+
+-- vim.g.floaterm_shell = "zsh"
+-- vim.g.floaterm_shell = "fish"
+-- vim.g.floaterm_shell = "bash"
