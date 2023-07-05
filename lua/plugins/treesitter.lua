747451a7102c6d44f97f0ee3283bf97496413cc3
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-07-05 11:51:30 -0300

    fixed conflicting keymaps

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	priority = 1,
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
@@ -17,13 +14,6 @@ return {
 			},
 			autopairs = {
 				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<S-u>",
-					node_incremental = "<S-u>",
-				},
 			},
 		})
 	end,
--- lua/user/keymaps/general_use_keymaps.lua
+++ lua/user/keymaps/general_use_keymaps.lua
@@ -8,8 +8,9 @@ Keymap("i", "jk", "<ESC>", Opts)
 -- Essentials
 Keymap("n", "<S-w>", ":w<CR>", Opts) -- save current buffer
 Keymap("n", "<S-x>", ":x<CR>", Opts) -- save and exit current buffer
-Keymap("n", "rr", ":redo<CR>", Opts) -- redo
+Keymap("n", "<S-q>", ":q<cr>", Opts) -- quit floating window
 Keymap("n", "<C-q>", ":qa!<cr>", Opts) -- quit all buffers without saving
+Keymap("n", "rr", ":redo<CR>", Opts) -- redo
 Keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/]], Opts) -- rename all instances of the thing under cursor on current buffer
 -- Comment Code
 Keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
