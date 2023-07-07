849d72ab0a7e39f6b96fe4edd19a110c692bb600
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-07-07 16:51:18 -0300

    Update custom_options.lua

--- lua/user/custom_options.lua
+++ lua/user/custom_options.lua
@@ -1,3 +1,4 @@
-vim.o.expandtab = false -- convert tabs to spaces
-vim.o.shiftwidth = 2 -- the number of spaces inserted for each indentation
-vim.o.tabstop = 2 -- insert 4 spaces for a tab
+-- Enter your custom options here, for example:
+-- vim.o.expandtab = false -- convert tabs to spaces
+-- vim.o.shiftwidth = 2 -- the number of spaces inserted for each indentation
+-- vim.o.tabstop = 2 -- insert 4 spaces for a tab
