17dd8dd372552b6909d7456e09c44b765a4b1106
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-08-02 10:12:34 -0300

    merged a commit made from the container and added
    More flutter-tools configurations (mostly enabling snippets).
    
    modified:   lua/plugins/flutter-tools.lua

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -5,4 +5,14 @@ return {
 		"stevearc/dressing.nvim", -- optional for vim.ui.select
 	},
 	config = true,
+	-- see the link below for details on each option:
+	-- https://github.com/dart-lang/sdk/blob/master/pkg/analysis_server/tool/lsp_spec/README.md#client-workspace-configuration
+	settings = {
+		showTodos = true,
+		completeFunctionCalls = true,
+		analysisExcludedFolders = { "$HOME/.flutter/bin/flutter" },
+		renameFilesWithClasses = "prompt", -- "always"
+		enableSnippets = true,
+		updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+	},
 }
