0c1a81826eca71c35cdb2c33df2ed46f2b179f30
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-10-20 10:59:02 -0300

    norminette keymap norm

--- lua/user/keymaps/general_use_keymaps.lua
+++ lua/user/keymaps/general_use_keymaps.lua
@@ -1,16 +1,14 @@
 -- load and use 42header plugin and norminetter:
 Keymap("n", "<F1>", ":Lazy load 42header<CR> | :Stdheader<CR>")
-Keymap("n", "<leader>h", ":Lazy load 42header<CR> <bar> :Stdheader<CR>") -- alternative 42header keymap
-Keymap("n", "<leader>cf", ":Lazy load CFormatter42<CR> | :CFormatter42<CR>") -- alternative CFormatter42
 Keymap("", "<Space>", "<Nop>", Opts) -- set leader key to space
 -- Normal Mode jk
 Keymap("i", "jk", "<ESC>", Opts)
 -- Essentials
-Keymap("n", "<S-w>", ":w<CR>", Opts) -- save current buffer
-Keymap("n", "<S-x>", ":x<CR>", Opts) -- save and exit current buffer
-Keymap("n", "<S-q>", ":q<cr>", Opts) -- quit floating window
-Keymap("n", "<C-q>", ":qa!<cr>", Opts) -- quit all buffers without saving
-Keymap("n", "rr", ":redo<CR>", Opts) -- redo
+Keymap("n", "<S-w>", ":w<CR>", Opts)                    -- save current buffer
+Keymap("n", "<S-x>", ":x<CR>", Opts)                    -- save and exit current buffer
+Keymap("n", "<S-q>", ":q<cr>", Opts)                    -- quit floating window
+Keymap("n", "<C-q>", ":qa!<cr>", Opts)                  -- quit all buffers without saving
+Keymap("n", "rr", ":redo<CR>", Opts)                    -- redo
 Keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/]], Opts) -- rename all instances of the thing under cursor on current buffer
 -- Comment Code
 Keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>')
@@ -20,3 +18,4 @@ Keymap(
 	":Lazy load Comment.nvim<CR> | <cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
 	Opts
 )
+Keymap("n", "norm", "<CMD>!norminette %<CR>", Opts);
--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,6 +1,7 @@
 local servers = {
 	"clangd",
 	"lua_ls",
+	"pyright"
 }
 
 require("mason").setup({
