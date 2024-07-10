9ac879a72ce3adc5c2c40f030b198bdf111f0061
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-07-10 13:29:26 -0300

    documented keymaps

--- lua/user/keymaps/telescope_keymaps.lua
+++ lua/user/keymaps/telescope_keymaps.lua
@@ -1,8 +1,13 @@
 -- Telescope
+-- Opens telescope on find-files mode:
 Keymap("n", "<leader>ff", ":Lazy load telescope-fzf-native.nvim<CR> | :Telescope find_files<CR>", Opts)
+-- Opens telescope on find-files mode from the current directory:
 Keymap("n", "<leader>fh", ":Telescope find_files cwd=~/ <CR>", Opts)
+-- Opens telescope on find-files mode from the root directory:
 Keymap("n", "<leader>fr", ":Telescope find_files cwd=/ <CR>", Opts)
+-- Navigate projects with telescope:
 Keymap("n", "<leader>fp", ":Telescope projects<CR>", Opts)
+-- Grep for a string in the current project:
 Keymap("n", "<leader>lg", ":Telescope live_grep<CR>", Opts)
 Keymap("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 Keymap("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
@@ -18,5 +23,5 @@ end, { desc = "Fuzzily [S]earch in current buffer" })
 
 Keymap("n", "mob", function()
 	require("telescope").load_extension("flutter")
-	require('telescope').extensions.flutter.commands()
+	require("telescope").extensions.flutter.commands()
 end, { desc = "opens flutter tools commands" })
--- lua/user/keymaps/vim-motion_tweaks.lua
+++ lua/user/keymaps/vim-motion_tweaks.lua
@@ -1,5 +1,4 @@
 Keymap("n", "x", '"_x') -- throws the deletion of a single char into the "black hole" (not the clipboard)
 Keymap("n", "J", "mzJ`z", Opts) -- makes the cursor stay on play when concatenating lines using "J"
-Keymap("n", "<C-d>", "<C-d>zz", Opts) -- center the screen while doing the <C-d> <C-u> thing
-Keymap("n", "<C-u>", "<C-u>zz", Opts)
-
+Keymap("n", "<C-d>", "<C-d>zz", Opts) -- centers the screen while doing the <C-d> <C-u> thing
+Keymap("n", "<C-u>", "<C-u>zz", Opts) -- centers the screen while doing the <C-d> <C-u> thing
